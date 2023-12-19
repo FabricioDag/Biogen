e8dbad768d89df3d18d64b0618905f927596ba87
Fabrício D'Agostini <115273639+FabricioDag@users.noreply.github.com>
2023-12-18 22:21:39 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -5,3 +5,9 @@ hamburguerBtn.addEventListener("click", function(){
     menu.classList.toggle("active")
     hamburguerBtn.classList.toggle("close")
 })
+
+window.addEventListener('scroll', function(){
+    var menus = document.querySelector('#menus');
+    menus.classList.toggle('ativo', window.scrollY > 0);
+    console.log('jaahah')
+  })
